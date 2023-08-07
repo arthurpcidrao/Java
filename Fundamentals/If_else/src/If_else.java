d8a0f89f747616aa210bb15e6b009a049ceb201e
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-08-07 20:23:39 -0300

    ok

---
+++ Fundamentals/If_else/src/If_else.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class If_else {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner var = new Scanner(System.in);
+        
+        System.out.print("Digite um numero entre 0 e 100: ");
+        int x = var.nextInt();
+
+        var.close();
+
+        if ((x >= 0) && (x <= 50)) {
+            System.out.format("O x (%d) esta entre 0 e 50", x);
+        }
+        else {
+            System.out.format("O x (%d) esta entre 51 e 100", x);
+        }
+
+    }
+}
