741701cc3e3f32b8bd556a18111618c10aa812a5
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-08-09 10:06:35 -0300

    set_function

---
+++ Fundamentals/set_function/src/Set_function.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Set_function {
+
+    // Criação de função fica fora da função principal (main)
+    public static int fatorial(int n){
+        int fator = 1;
+        for (int i = 1; i <= n; i++) {
+            fator = i*fator;
+        }
+        return fator;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite um número e receba seu fatorial: ");
+        int fat = input.nextInt();
+
+        input.close();
+
+        int resp;
+        resp = fatorial(fat);
+        System.out.format("O fatorial de %d eh %d!", fat, resp);
+        
+    }
+}
