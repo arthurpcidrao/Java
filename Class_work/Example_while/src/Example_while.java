1a8d213157875a8654ef06ccdadb7a264b134938
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-08-16 20:38:21 -0300

    Update Example_while.java

--- Class_work/Example_while/src/Example_while.java
+++ Class_work/Example_while/src/Example_while.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Example_while {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int age = 0, qtd = 0;
+        double average, sum = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        while (age >= 0) {
+            
+            age = input.nextInt();
+            
+            if (age >= 0) {
+                qtd++;
+                sum = sum + age;
+            }
+        }
+        input.close();
+        average = sum/(qtd);
+
+        System.out.printf("%.2f\n", average);
+
     }
 }
