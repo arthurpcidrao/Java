69887f289bf74e20a51f7dae616a915d922246d0
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-08-23 09:47:18 -0300

    experiments

---
+++ Class_work/Experiments/src/Experiments.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Experiments {
+    public static void main(String[] args) throws Exception {
+        
+        int num;
+        Scanner input = new Scanner(System.in);
+
+        num = input.nextInt();
+        
+
+        
+    }
+}
