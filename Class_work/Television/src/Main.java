d11c6116f7766581e71697d6d96f74618a6e5332
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-08-30 20:45:11 -0300

    poo

--- Class_work/Television/src/Main.java
+++ Class_work/Television/src/Main.java
@@ -3,40 +3,48 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        char funcaoControle = 'f';
+        int funcaoControle = 9;
         Scanner input = new Scanner(System.in);
 
         Televisao tv = new Televisao();
         Controle ctrl = new Controle(tv);  // estou dizendo que o controle (ctrl) vai poder controlar o objeto tv
         
         System.out.println("Comandos do controle:");
-        System.out.print("o: ligar/desligar\nr: aumentar volume\nl: abaixar volume\nu: aumentar canal");
-        System.out.print("\nd: diminui canal\ns: finaliza a execucao");
+        System.out.print("0: encerra o programa");
+        System.out.print("\n1: liga a tv\n2: aumenta volume\n3: diminui volume\n4: aumenta canal\n5: diminui canal");
+        System.out.print("\n6: coloque números de 1 a 15 para poder ir direto para o canal de interesse\n\n");
 
         // botões do controle
-        while (funcaoControle != 's'){
-            funcaoControle = input.next().charAt(0);
-            if (funcaoControle == 'o'){
+        while (funcaoControle != 0){
+
+            System.out.print("Controle: ");
+            funcaoControle = input.nextInt();
+            if (funcaoControle == 1){
                 ctrl.tvOn();
             }
 
-            if (funcaoControle == 'r'){
+            if (funcaoControle == 2){
                 ctrl.volumeUp();
             }
 
-            if (funcaoControle == 'l'){
+            if (funcaoControle == 3){
                 ctrl.volumeDown();
             }
 
-            if (funcaoControle == 'u'){
+            if (funcaoControle == 4){
                 ctrl.channelUp();
             }
 
-            if (funcaoControle == 'd'){
+            if (funcaoControle == 5){
                 ctrl.channelDown();
             }
 
+            if (funcaoControle == 6){
+                int canal = input.nextInt();
+                ctrl.changeChannel(canal);
+            }
 
+            tv.imprima();
 
         }
 
--- Class_work/Television/src/Televisao.java
+++ Class_work/Television/src/Televisao.java
@@ -17,6 +17,7 @@ public class Televisao {
         if (this.tvOn == false){
             this.tvOn = true;
         }
+        System.out.println("Televisao LIGADA");
         return tvOn;
     }
 
@@ -24,38 +25,36 @@ public class Televisao {
         if (this.tvOn == true){
             this.tvOn = false;
         }
+        System.out.println("Televisao DESLIGADA");
         return tvOn;
     }
 
-    public boolean getTv(){
-        return this.tvOn;
-    }
-
     public void volumeUp(){
-        if (this.volume < 100){
+        if (this.volume < 100 && tvOn){
             this.volume++;
         }
-        System.out.printf("Volume: %d", this.volume);
     }
 
     public void volumeDown(){
-        if (this.volume > 0){
+        if (this.volume > 0 && tvOn){
             this.volume--;
         }
-        System.out.printf("Volume: %d", this.volume);
     }
 
     public void channelUp(){
-        if (this.channel < 15){
+        if (this.channel < 15 && tvOn){
             this.channel++;
         }
-        System.out.printf("Canal: %d", this.channel);
     }
 
     public void channelDown(){
-        if (this.channel > 0){
+        if (this.channel > 0 && tvOn){
             this.channel--;
         }
-        System.out.printf("Canal: %d", this.channel);
+    }
+
+    public void imprima(){
+        System.out.printf("Canal: %d\n", this.channel);
+        System.out.printf("Volume: %d\n\n", this.volume);
     }
 }
