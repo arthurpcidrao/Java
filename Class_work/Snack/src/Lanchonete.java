5ac947bfbe8d8601a79dc21678f72d90f7d05f73
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-09-09 12:41:25 -0300

    lanchonete

--- Class_work/Snack/src/Lanchonete.java
+++ Class_work/Snack/src/Lanchonete.java
@@ -1,3 +1,54 @@
 public class Lanchonete {
+    // atributos
+    private int lanche;
+    private int qtd;
     
+    // métodos
+
+    public Lanchonete(int lanche, int qtd){
+        this.lanche = lanche;
+        this.qtd = qtd;
+    }
+
+    public int getLanche(){
+        return lanche;
+    }
+
+    public void setLanche(int lanche){
+        this.lanche = lanche;
+    }
+
+    public int getQtd(){
+        return qtd;
+    }
+
+    public void setQtd(int qtd){
+        this.qtd = qtd;
+    }
+
+    public double valorFinal(){
+        double preco = 0;
+
+        if (this.lanche == 1){
+            preco = 4.00;
+        }
+
+        else if (this.lanche == 2){
+            preco = 4.50;
+        }
+
+        else if (this.lanche == 3){
+            preco = 5.00;
+        }
+
+        else if (this.lanche == 4){
+            preco = 2.00;
+        }
+
+        else if (this.lanche == 5){
+            preco = 1.50;
+        }
+
+        return preco * this.qtd;
+    }
 }
--- Class_work/Snack/src/Main.java
+++ Class_work/Snack/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         
+        Scanner input = new Scanner(System.in);
+
+        int codigo = input.nextInt();
+        int quantidade = input.nextInt();
+
+        input.close();
+
+        Lanchonete lan = new Lanchonete(codigo, quantidade);
+
+        double total = lan.valorFinal();
+
+        System.out.printf("Total: R$ %.2f\n", total);
+
     }
 }
