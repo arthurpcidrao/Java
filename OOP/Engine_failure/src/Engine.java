5a5427b9a493901d768747beb29df8189d3b4bc1
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-10-21 12:03:07 -0300

    a

---
+++ OOP/Engine_failure/src/Engine.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Engine {
+    private ArrayList<Integer> testes = new ArrayList();
+    private int rpm;
+
+    public Engine (){
+        System.out.println("passou aqui");
+    }
+
+    public int getRpm() {
+        return rpm;
+    }
+    
+    public void setRpm(int rpm) {
+        this.rpm = rpm;
+    }
+
+    public void engineMeasures(int n){
+        
+        for (int i = 0; i < n; i++){
+
+
+        }
+    }
+}
--- OOP/Engine_failure/src/Main.java
+++ OOP/Engine_failure/src/Main.java
@@ -1,5 +1,8 @@
-public class App {
+import java.util.*;
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
