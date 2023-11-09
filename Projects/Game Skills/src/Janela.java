a5975f3167f2b7c77ed16140bf15e0307aaff224
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-11-09 20:35:43 -0300

    Tetris

---
+++ Projects/Game Skills/src/Janela.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Janela extends JFrame {
+    
+    public Janela(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(640, 480);
+        setVisible(true);
+    }
+}
