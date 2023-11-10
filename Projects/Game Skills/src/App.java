9e3cd3e0056aa452eb447052e99cf67d3295c4a4
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-11-10 17:39:26 -0300

    graphics learning

--- Projects/Game Skills/src/App.java
+++ Projects/Game Skills/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        new ScreenAnimation(); //abre uma moldura a partir de JFrame
+        ScreenAnimation animacao = new ScreenAnimation(); //abre uma moldura a partir de JFrame
+        animacao.beginAnimation();
 
     }
 }
--- Projects/Game Skills/src/ScreenAnimation.java
+++ Projects/Game Skills/src/ScreenAnimation.java
@@ -5,24 +5,31 @@ import javax.swing.JPanel;  // criação de pinturas
 
 public class ScreenAnimation extends JFrame {
     private JPanel tela;
+    private int fps = 1000/20;  // 1000 milissegundos, 20 movimentações (em 1 segundo, movimentar 20 vezes)
+    private int count;
+    private boolean anima = true;
     
     public ScreenAnimation() {
         tela = new JPanel() {
             @Override
             public void paintComponent(Graphics g){
                 // a pintura acontece aqui
+
+                g.setColor(Color.WHITE);
+                g.fillRect(0, 0, tela.getWidth(), tela.getHeight());
+
                 g.setColor(Color.BLUE);
-                g.drawLine(0, 240, 640, 240);               // desenha linha
-                g.drawRect(10, 25, 20, 20);            // desenha um quadrado
-                g.drawOval(30, 20, 40, 30);            // desenha oval
+                g.drawLine(0, 240 + count, 640, 240 + count);               // desenha linha
+                g.drawRect(10, 25 + count, 20, 20);            // desenha um quadrado
+                g.drawOval(30 + count, 20, 40, 30);            // desenha oval
 
                 g.setColor(Color.YELLOW);
-                g.drawLine(320, 0, 320, 480);
-                g.fillRect(110, 125, 120, 120);        // preenche quadrado
-                g.fillOval(230, 220, 240, 230);        // preenche oval
+                g.drawLine(320 - count, 0, 320 - count, 480);
+                g.fillRect(110, 125, 120 - count, 120 - count);        // preenche quadrado
+                g.fillOval(230, 220, 240 + count, 230);        // preenche oval
 
                 g.setColor(Color.RED);
-                g.drawString("Eu seria um ótimo Score",5, 10);
+                g.drawString("Eu seria um ótimo Score" + count,5, 10);
             }
         };
 
@@ -32,4 +39,27 @@ public class ScreenAnimation extends JFrame {
         setVisible(true);
         tela.repaint();
     }
+
+    public void beginAnimation(){
+        long proxAtualizacao = 0;
+
+        while(anima){
+            if (System.currentTimeMillis() >= proxAtualizacao){
+                count++;
+                tela.repaint();
+
+                proxAtualizacao = System.currentTimeMillis() + fps;
+
+                if (count == 100){
+                    anima = false;
+                }
+            }
+        }
+    }
+
+
+
+
+
+
 }
