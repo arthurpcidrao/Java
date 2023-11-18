6b5a6642b7af7ba2e5c6c2c8f31743b5048a88f3
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2023-11-18 18:11:41 -0300

    tetris arthur

--- Projects/First Game/src/UmJogo.java
+++ Projects/First Game/src/UmJogo.java
@@ -158,7 +158,7 @@ public class UmJogo extends JFrame {
                 }
                 else{
                     bloco.y += bloco.velocidade;
-                }
+                    }
             }
 
             pontos = pontos + blocos.length;
--- Projects/Tetris/src/AppJogo.java
+++ Projects/Tetris/src/AppJogo.java
@@ -2,12 +2,33 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import java.util.Random;
+
 public class AppJogo {
     public static void main(String[] args) throws Exception {
+        boolean jogando = true;
+        boolean fimJogo = false;
         
-        Tela jogo = new Tela(800, 672);
+        // ------------------- DEFINIÇÕES DA TELA ------------------- //
+        JFrame frame = new JFrame("Tetris do Arthur Paraiba");
+        frame.setSize(800, 672);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        frame.setContentPane(panel);
+
         JLabel rotulo = new JLabel();
         rotulo.setText("olá");
+        rotulo.setForeground(Color.WHITE);
+        rotulo.setFont(new Font("Tahoma",Font.BOLD, 15));
+
+        
+        panel.add(rotulo);
+        panel.setBackground(Color.DARK_GRAY);
         
 
     }
