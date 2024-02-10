086783adde07fff577521213a7e26a5d964e4969
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2024-02-10 18:08:59 -0300

    Recursion

--- Fundamentals/Recursion 7/src/App.java
+++ Fundamentals/Recursion 7/src/App.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Placar m x n, time A x B");
+
+        System.out.print("m = ");
+        int golsA = input.nextInt();
+
+        System.out.print("n = ");
+        int golsB = input.nextInt();
+
+        input.close();
+        permutacoes("", golsA, golsB);
+        
+    }
+
+    // algoritmo DFS (Depth-first Search) -> busca em profundidade
+    public static void permutacoes(String caso, int a, int b){
+        if (a == 0 && b == 0){
+            //System.out.println("finalizou");
+            System.out.println(caso);
+            //System.out.println();
+        }
+        if (a > 0){
+            //System.out.printf("Entrou em A = %d\n", a);
+            permutacoes(caso + "A", a-1, b);
+        }
+        if (b > 0) {
+            //System.out.printf("Entrou em B = %d\n", b);
+            permutacoes(caso + "B", a, b-1);
+        }
     }
 }
