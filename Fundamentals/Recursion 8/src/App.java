3cf4a7a47f289fc21c71a5dfbdad902d2d54b068
Arthur Paraiba Cidrão <139405105+arthurpcidrao@users.noreply.github.com>
2024-02-18 12:12:44 -0300

    Recursion

--- Fundamentals/Recursion 8/src/App.java
+++ Fundamentals/Recursion 8/src/App.java
@@ -4,45 +4,28 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("O Problema de Josefo:");
-        System.out.print("tamanho da fila: ");
-        int num = input.nextInt();
+        int cases = input.nextInt();
+        //System.out.println("O Problema de Josefo:");
+        //System.out.print("tamanho da fila: ");
+        //int n = input.nextInt();
 
-        int roda[] = new int[num];
-        for (int i = 0; i < num; i++){
-            roda[i] = i;
+        //System.out.print("De quanto será pulado? ");
+        //int k = input.nextInt();
+
+        for (int i = 0; i < cases; i++){
+            int n = input.nextInt();
+            int k = input.nextInt();
+            System.out.printf("Case %d: %d\n", i+1, josephus(n,k)+1);
         }
-
-        System.out.print("De quanto será pulado? ");
-        int passo = input.nextInt();
-
-        int count = 0;
-        int finalista = -1;
-        int inicio = 0;
-        int test = 0;
-        while(count < roda.length){
-
-            while (inicio < roda.length){
-                roda[inicio] = -1;
-                inicio = inicio + passo;
-            }
-            inicio = inicio - (roda.length - 1);
-
-            test = 0;
-            for (int i = 0; i < roda.length; i++){
-                count++;
-                //System.out.println(roda[i]);
-                if(roda[i] != -1){
-                    finalista = roda[i];
-                    test++;
-                    System.out.println(test);
-                    count--;
-                }
-            }
-        }
-
-        System.out.printf("O sobrevivente foi o número %d", finalista);
-
         input.close();
+    }
+
+    public static int josephus(int n, int k){
+        
+        if (n == 1){
+            return 0;
+        }
+
+        return (josephus(n-1,k) + k)%n;
     }
 }
